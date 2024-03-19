5926c4650f4c2efae6bd1bfe0224ee1f8d977bc8
Kevin Paiz Ramos <paizkevin27@gmail.com>
2024-03-19 15:28:00 -0400

    Initial commit

---
+++ Movies/src/main/java/dev/kev/Movies/ReviewService.java
@@ -0,0 +1,37 @@
+package dev.kev.Movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.data.mongodb.core.aggregation.SelectionOperators.First.first;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review.getId()))
+                .first();
+        return review;
+    }
+}
